698de0f9766c00aad72856e4b8a4bccbe2d92589
BSI80207 <boanfourierr@gmail.com>
2024-03-14 15:08:44 +0700

    Task 1_Petrus

---
+++ MyWebFormApp.API/Controllers/ArticlesController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+using MyWebFormApp.BO;
+
+namespace MyRESTServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticlesController : ControllerBase
+    {
+        private readonly IArticleBLL _articleBLL;
+        public ArticlesController(IArticleBLL articleBLL)
+        {
+            _articleBLL = articleBLL;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _articleBLL.GetArticleWithCategory();
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            if (result.Count() == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _articleBLL.GetArticleById(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _articleBLL.Delete(id);
+                return Ok("Success Delete Article");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ArticleCreateDTO articleDTO)
+        {
+            try
+            {
+                _articleBLL.Insert(articleDTO);
+                return Ok("Article created successfully");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ArticleUpdateDTO articleDTO)
+        {
+            try
+            {
+                var article = _articleBLL.GetArticleById(id);
+                if (article == null)
+                {
+                    return NotFound();
+                }
+
+                articleDTO.ArticleID = id;
+                _articleBLL.Update(articleDTO);
+                return Ok("Article update successful");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetArticleByCategory/{id}")]
+        public IActionResult GetArticleByCategory(int id)
+        {
+            var result = _articleBLL.GetArticleByCategory(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            if (result.Count() == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
---
+++ MyWebFormApp.API/Controllers/CategoriesController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.DTOs;
+using MyWebFormApp.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyRESTServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryBLL _categoryBLL;
+        public CategoriesController(ICategoryBLL categoryBLL)
+        {
+            _categoryBLL = categoryBLL;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _categoryBLL.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var result = _categoryBLL.GetById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _categoryBLL.Delete(id);
+                return Ok("Success Delete Category");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CategoryCreateDTO categoryDTO)
+        {
+            try
+            {
+                _categoryBLL.Insert(categoryDTO);
+                return Ok("Category created successfully");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] CategoryUpdateDTO categoryDTO)
+        {
+            try
+            {
+                var category = _categoryBLL.GetById(id);
+                if (category == null)
+                {
+                    return NotFound();
+                }
+                categoryDTO.CategoryID = id;
+                _categoryBLL.Update(categoryDTO);
+                return Ok("Category update successful");
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetByName/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var result = _categoryBLL.GetByName(name);
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
